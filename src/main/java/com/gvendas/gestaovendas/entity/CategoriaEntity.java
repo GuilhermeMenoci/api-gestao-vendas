b12745a74b39ac0e83acacb008b4a078f8c3efca
Guilherme Menoci <guimenoci@gmail.com>
2022-01-07 18:47:15 -0300

    Criação do projeto e criando a entidade Categoria

---
+++ src/main/java/com/gvendas/gestaovendas/entity/CategoriaEntity.java
@@ -0,0 +1,61 @@
+package com.gvendas.gestaovendas.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class CategoriaEntity implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigo")
+	private Long codigo;
+	
+	@Column(name = "nome")
+	private String nome;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaEntity other = (CategoriaEntity) obj;
+		return Objects.equals(codigo, other.codigo) && Objects.equals(nome, other.nome);
+	}
+	
+	
+}
